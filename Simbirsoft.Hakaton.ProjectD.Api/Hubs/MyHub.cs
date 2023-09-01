30c413a7e1072c58a0a0aac23bad7c49a85d7191
Alexander Kharkovskiy <alexander.kharkovskiy@simbirsoft.com>
2023-09-01 16:14:18 +0400

    add signalr

---
+++ Simbirsoft.Hakaton.ProjectD.Api/Hubs/MyHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Simbirsoft.Hakaton.ProjectD.Api.Hubs;
+
+public class MyHub : Hub
+{
+    public Task Broadcast()
+    {
+        return Clients.All.SendAsync("test", new[] { "hello", "world" });
+    }
+}
--- Simbirsoft.Hakaton.ProjectD.Api/Program.cs
+++ Simbirsoft.Hakaton.ProjectD.Api/Program.cs
@@ -1,7 +1,9 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Simbirsoft.Hakaton.ProjectD.Api.Hubs;
 using Simbirsoft.Hakaton.ProjectD.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,15 +16,23 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
 builder.Services.AddAuthorizationBuilder();
 
+builder.Services.AddSignalR().AddJsonProtocol(options =>
+{
+    options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+    options.PayloadSerializerOptions.Converters
+        .Add(new JsonStringEnumConverter());
+});
+
 var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
 
-builder.Services.AddIdentityCore<User>().AddMongoDbStores<User, Role, Guid>(mongoDbSettings.ConnectionString, mongoDbSettings.Name).AddApiEndpoints();
+builder.Services.AddIdentityCore<User>()
+    .AddMongoDbStores<User, Role, Guid>(mongoDbSettings.ConnectionString, mongoDbSettings.Name).AddApiEndpoints();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.MapIdentityApi<User>();
+app.MapGroup("api/v1").MapIdentityApi<User>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,6 +41,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .WithOrigins()
+    .AllowCredentials()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseRouting();
+
 app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    endpoints.MapHub<MyHub>("/hubs/myhub");
+});
 
 app.Run();
