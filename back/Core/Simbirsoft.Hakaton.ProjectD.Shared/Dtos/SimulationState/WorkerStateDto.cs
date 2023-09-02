809b08766f2269f01a98958014fde0b8627ccf41
deminik <n.i.demidov@gmail.com>
2023-09-02 14:23:12 +0400

    добавил DTO состояния

---
+++ back/Core/Simbirsoft.Hakaton.ProjectD.Shared/Dtos/SimulationState/WorkerStateDto.cs
@@ -0,0 +1,14 @@
+﻿using Simbirsoft.Hakaton.ProjectD.Shared.Dtos.Map;
+
+namespace Simbirsoft.Hakaton.ProjectD.Shared.Dtos.SimulationState;
+
+public class WorkerStateDto
+{
+    public string Id { get; set; }
+
+    public CoordinateDto Coordinate { get; set; }
+    
+    public string TargetId { get; set; }
+
+    public int Damage { get; set; }
+}
