c53dc358bc77cb42d302f7760e97bdf3bcdc7d06
deminik <n.i.demidov@gmail.com>
2023-09-02 18:12:29 +0400

    добавил нумерацию видов воркеров

--- back/Core/Simbirsoft.Hakaton.ProjectD.Shared/Enums/Workers/WorkerType.cs
+++ back/Core/Simbirsoft.Hakaton.ProjectD.Shared/Enums/Workers/WorkerType.cs
@@ -2,13 +2,13 @@
 
 public enum WorkerType
 {
-    Undefined,
-    BackendDev,
-    FrontendDev,
-    TeamLead,
-    Analyst,
-    ProjectManager,
-    QA,
-    DBA,
-    Designer
+    Undefined = 0,
+    BackendDev = 1,
+    FrontendDev = 2,
+    TeamLead = 3,
+    Analyst = 4,
+    ProjectManager = 5,
+    QA = 6,
+    DBA = 7,
+    Designer = 8
 }
