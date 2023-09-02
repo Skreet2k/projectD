f36db7a6e6fea0d026fc6e576e3f45da0ef74f1c
deminik <n.i.demidov@gmail.com>
2023-09-02 12:42:00 +0400

    долил хэлпер

---
+++ back/Core/Simbirsoft.Hakaton.ProjectD.Shared/Helpers/GeometryHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Simbirsoft.Hakaton.ProjectD.Shared.Helpers;
+
+public static class GeometryHelper
+{
+    public static bool IsInRange(int sourceX, int sourceY, int range, int targetX, int targetY)
+    {
+        var targetRadius = Math.Sqrt(Math.Pow(targetX - sourceX, 2) + Math.Pow(targetY - sourceY,2));
+
+        return range >= targetRadius;
+    }
+}
