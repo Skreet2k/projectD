e7210e904f43baeb57e220845387098709ba4fe2
deminik <n.i.demidov@gmail.com>
2023-09-02 12:39:50 +0400

    добавил определение расстояния, тесты

---
+++ back/Tests/Simbirsoft.Hakaton.ProjectD.Simulator.Tests/GeometryTests.cs
@@ -0,0 +1,29 @@
+﻿using Simbirsoft.Hakaton.ProjectD.Shared.Helpers;
+
+namespace Simbirsoft.Hakaton.ProjectD.Simulator.Tests;
+
+public class GeometryTests
+{
+    [Theory]
+    [InlineData(0, 0, 2, 1, 1)]
+    [InlineData(1, 1, 2, 2, 2)]
+    [InlineData(2, 2, 2, 1, 1)]
+    public void Target_IsInRangeRange(int sourceX, int sourceY, int range, int targetX, int targetY)
+    {
+        var isInRange = GeometryHelper.IsInRange(sourceX, sourceY, range, targetX, targetY);
+
+        Assert.True(isInRange);
+    }
+
+    [Theory]
+    [InlineData(0, 0, 2, 2, 2)]
+    [InlineData(1, 1, 2, 3, 3)]
+    [InlineData(-1, -1, 2, 3, 3)]
+    [InlineData(3, 3, 2, 0, 0)]
+    public void Target_OutOfRange(int sourceX, int sourceY, int range, int targetX, int targetY)
+    {
+        var isInRange = GeometryHelper.IsInRange(sourceX, sourceY, range, targetX, targetY);
+
+        Assert.False(isInRange);
+    }
+}
