b561bd4f69bc5ab0c2babd3fecb1847c36312612
deminik <n.i.demidov@gmail.com>
2023-09-02 13:22:13 +0400

    прописал логику башни

--- back/Core/Simbirsoft.Hakaton.ProjectD.Simulator/Models/FeatureModel.cs
+++ back/Core/Simbirsoft.Hakaton.ProjectD.Simulator/Models/FeatureModel.cs
@@ -8,13 +8,34 @@ public class FeatureModel
     public CoordinateDto CurrentCoordinate { get; set; }
     public CoordinateDto NextCoordinate { get; set; }
     public int ProgressPercents { get; set; }
-    
+
     public int CurrentHealthPoints { get; set; }
     public int MaxHealthPoints { get; set; }
-    
+
     public string Name { get; set; }
-    
+
     public int ProgressPerTick { get; set; }
-    
+
     public int Reward { get; set; }
+
+    /// <summary>
+    /// Применить нанесённый урон.
+    /// </summary>
+    /// <param name="damage">Нанесённый урон.</param>
+    public void ReceiveDamage(int damage)
+    {
+        // Вычитаем урон из текущего ХП.
+        CurrentHealthPoints -= damage;
+
+        // Если фича опустилась до 0 ХП, он выполняется.
+        if (CurrentHealthPoints <= 0)
+            OnComplete();
+    }
+
+    /// <summary>
+    /// Событие завершения фичи.
+    /// </summary>
+    private void OnComplete()
+    {
+    }
 }
