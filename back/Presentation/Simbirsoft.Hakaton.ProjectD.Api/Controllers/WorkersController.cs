344ba644af0bd6cb2f7d5386febaaf9e970f7abb
Alexander Kharkovskiy <alexander.kharkovskiy@simbirsoft.com>
2023-09-03 15:37:18 +0400

    Add worker

--- back/Presentation/Simbirsoft.Hakaton.ProjectD.Api/Controllers/WorkersController.cs
+++ back/Presentation/Simbirsoft.Hakaton.ProjectD.Api/Controllers/WorkersController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Simbirsoft.Hakaton.ProjectD.Application.Services;
+using Simbirsoft.Hakaton.ProjectD.Domain.Abstractions.Services;
 using Simbirsoft.Hakaton.ProjectD.Domain.Abstractions.Services.Workers;
 using Simbirsoft.Hakaton.ProjectD.Shared.Dtos.Workers;
 using Skreet2k.Common.Models;
@@ -12,15 +16,39 @@ namespace Simbirsoft.Hakaton.ProjectD.Api.Controllers;
 public class WorkersController : ControllerBase
 {
     private readonly IWorkersService _workersService;
+    private readonly ISimulationSessionService _simulationSessionService;
 
-    public WorkersController(IWorkersService workersService)
+    public WorkersController(IWorkersService workersService, ISimulationSessionService simulationSessionService)
     {
         _workersService = workersService;
+        _simulationSessionService = simulationSessionService;
     }
 
     [HttpGet]
     public ResultList<WorkerDto> GetWorkers()
     {
         return _workersService.GetWorkers();
+    }
+    
+    /// <summary>
+    /// Добавляем работника.
+    /// </summary>
+    [HttpPost]
+    public void AddWorker(WorkerUpsetDto worker)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        _simulationSessionService.AddWorker(userId, worker.Id, worker.Coordinate);
+    }
+        
+    /// <summary>
+    /// Удаляем работника.
+    /// </summary>
+    [HttpDelete]
+    public void RemoveWorker(WorkerUpsetDto worker)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        _simulationSessionService.RemoveWorker(userId, worker.Id, worker.Coordinate);
     }
 }
