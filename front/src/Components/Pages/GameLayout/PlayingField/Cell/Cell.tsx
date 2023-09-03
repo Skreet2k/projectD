1eaaaea06c72517eb26d852d0ab309d5bddc3dcf
Iaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-09-03 17:03:27 +0400

    add addWorker

--- front/src/Components/Pages/GameLayout/PlayingField/Cell/Cell.tsx
+++ front/src/Components/Pages/GameLayout/PlayingField/Cell/Cell.tsx
@@ -5,6 +5,7 @@ import { CellDiv } from './Cell.styles';
 import { RootState } from '../../../../../store/store';
 import { GameLayoutContext } from '../../../../Providers/GameLayoutProvider/GameLayoutProvider';
 import { setSelectedShopTower } from '../../../../../store/slices/gameLayoutSlice';
+import { useAddTowerMutation } from '../../../../../services/towers/towers';
 
 function Cell({ cell, cellSize }: CellProps) {
   const dispatch = useDispatch();
@@ -12,10 +13,11 @@ function Cell({ cell, cellSize }: CellProps) {
   const shopTowerSelected = useSelector(
     (state: RootState) => state.gameLayout.shopTowerSelected,
   );
+  const [addTower] = useAddTowerMutation();
 
   const onClick = () => {
     if (shopTowerSelected) {
-      socket?.addWorker({ workerId: shopTowerSelected.id, coordinate: { X: cell.position.x, Y: cell.position.y } });
+      addTower({ id: shopTowerSelected.id, coordinate: { X: cell.position.x, Y: cell.position.y } });
       dispatch(setSelectedShopTower({ id: '' }));
     }
   };
