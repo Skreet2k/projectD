1dd8f8d802922e64b73ca48cd813fd2983386087
deminik <n.i.demidov@gmail.com>
2023-09-03 18:53:44 +0400

    вынес конфиг

--- back/Core/Simbirsoft.Hakaton.ProjectD.Application/Services/SimulationSessionService.cs
+++ back/Core/Simbirsoft.Hakaton.ProjectD.Application/Services/SimulationSessionService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Options;
 using Simbirsoft.Hakaton.ProjectD.Domain.Abstractions.Services;
 using Simbirsoft.Hakaton.ProjectD.Domain.Abstractions.Services.Map;
 using Simbirsoft.Hakaton.ProjectD.Domain.Abstractions.Services.Workers;
+using Simbirsoft.Hakaton.ProjectD.Persistence.Configurations;
 using Simbirsoft.Hakaton.ProjectD.Shared.Dtos.Map;
 using Simbirsoft.Hakaton.ProjectD.Simulator.Abstractions;
 using Simbirsoft.Hakaton.ProjectD.Simulator.Models;
@@ -11,31 +13,35 @@ namespace Simbirsoft.Hakaton.ProjectD.Application.Services;
 /// <inheritdoc />
 public class SimulationSessionService : ISimulationSessionService
 {
-    private const int X = 8;
-    private const int Y = 6;
     private static readonly ConcurrentDictionary<string, SimulationModel> UserSessions = new();
     private readonly IMapGenerator _mapGenerator;
     private readonly ISimulationStarter _simulationStarter;
     private readonly IWorkersService _workersService;
+    private readonly GameConfiguration _gameConfig;
 
     public SimulationSessionService(
         IMapGenerator mapGenerator,
         ISimulationStarter simulationStarter,
-        IWorkersService workersService)
+        IWorkersService workersService,
+        IOptions<GameConfiguration> gameConfig)
     {
         _mapGenerator = mapGenerator;
         _simulationStarter = simulationStarter;
         _workersService = workersService;
+        _gameConfig = gameConfig.Value;
     }
 
     /// <inheritdoc />
     public async Task<MapDto> CreateSessionAsync(string userId)
     {
+        var X = _gameConfig.MapWidth;
+        var Y = _gameConfig.MapHeight;
+
         var rand = new Random();
         var startY = rand.Next(Y);
         var endY = rand.Next(Y);
 
-        var mapResult = await _mapGenerator.GenerateMapAsync(X, Y, 0, (byte)startY, 7, (byte)endY);
+        var mapResult = await _mapGenerator.GenerateMapAsync((byte)X, (byte)Y, 0, (byte)startY, 7, (byte)endY);
 
         var levelPool = new List<FeatureModel>();
 
@@ -44,13 +50,8 @@ public class SimulationSessionService : ISimulationSessionService
             Path = mapResult.Content.Path,
             Features = new List<FeatureModel>(),
             Workers = new List<WorkerModel>(),
-            Configuration = new SimulationConfiguration
-            {
-                IsEndlessLevel = true,
-                TicksToSpawn = 2,
-                MillisecondsToTick = 200
-            },
-            Money = 100
+            Configuration = _gameConfig,
+            Money = _gameConfig.StartMoney
         };
 
         mapModel.Customer = new CustomerModel(mapModel, levelPool);
---
+++ back/Infrastructure/Simbirsoft.Hakaton.ProjectD.Persistence/Configurations/GameConfiguration.cs
@@ -0,0 +1,104 @@
+﻿namespace Simbirsoft.Hakaton.ProjectD.Persistence.Configurations;
+
+/// <summary>
+/// Настройки игры.
+/// </summary>
+public class GameConfiguration
+{
+    /// <summary>
+    /// Секция по умолчанию.
+    /// </summary>
+    public const string DefaultSection = nameof(GameConfiguration);
+
+    /// <summary>
+    /// Настройки волн.
+    /// </summary>
+    public WaveConfiguration WaveConfiguration { get; set; }
+
+    /// <summary>
+    /// Настройки фич.
+    /// </summary>
+    public FeatureConfiguration FeatureConfiguration { get; set; }
+
+    /// <summary>
+    /// Ширина карты.
+    /// </summary>
+    public int MapWidth { get; set; }
+
+    /// <summary>
+    /// Высота карты.
+    /// </summary>
+    public int MapHeight { get; set; }
+
+    /// <summary>
+    /// Бесконечный уровень.
+    /// </summary>
+    public bool IsEndlessLevel { get; set; }
+
+    /// <summary>
+    /// Количество тиков между спавном фич.
+    /// </summary>
+    public int TicksToSpawn { get; set; }
+
+    /// <summary>
+    /// Количество миллисекунд междду тиками.
+    /// </summary>
+    public int MillisecondsToTick { get; set; }
+
+    /// <summary>
+    /// Стартовый капитал.
+    /// </summary>
+    public int StartMoney { get; set; }
+}
+
+/// <summary>
+/// Конфигурация волн.
+/// </summary>
+public class WaveConfiguration
+{
+    /// <summary>
+    /// Количество фичей в волне.
+    /// </summary>
+    public int WaveCapacity { get; set; }
+
+    /// <summary>
+    /// Модификатор скорости волны.
+    /// </summary>
+    public double WaveSpeedModifier { get; set; }
+
+    /// <summary>
+    /// Модификатор здоровья волны.
+    /// </summary>
+    public double WaveHealthModifier { get; set; }
+
+    /// <summary>
+    /// Модификатор награды волны.
+    /// </summary>
+    public double WaveRewardModifier { get; set; }
+
+    /// <summary>
+    /// Модификатор очков волны.
+    /// </summary>
+    public double WaveScoreModifier { get; set; }
+}
+
+/// <summary>
+/// Настройка фич.
+/// </summary>
+public class FeatureConfiguration
+{
+    /// <summary>
+    /// Начальные ХП.
+    /// </summary>
+    public int InitHealth { get; set; }
+
+    /// <summary>
+    /// Начальная скорость.
+    /// </summary>
+    public int InitSpeed { get; set; }
+
+    /// <summary>
+    /// Начальная награда.
+    /// </summary>
+    public int InitReward { get; set; }
+}
