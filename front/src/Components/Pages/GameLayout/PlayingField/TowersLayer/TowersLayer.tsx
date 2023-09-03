cea66efed807a755feeffff2467ab692a0c52072
Iaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-09-03 16:01:50 +0300

    Match workers received from socket

--- front/src/Components/Pages/GameLayout/PlayingField/TowersLayer/TowersLayer.tsx
+++ front/src/Components/Pages/GameLayout/PlayingField/TowersLayer/TowersLayer.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Tower as ITower } from '../../../../../services/towers/towers';
+import { Tower as ITower, useGetTowersQuery } from '../../../../../services/towers/towers';
 import Tower from './Tower/Tower';
 import { TWorker } from '../../../../../api/useSocketData/useSocketData.types';
-import { DeveloperLevel, TowerType } from './TowerLayer.types';
 
 type TowersLayerProps = {
   workers: TWorker[];
 };
 function TowersLayer({ workers }: TowersLayerProps) {
+  const { data } = useGetTowersQuery();
+
+  if (!data) {
+    return null;
+  }
+  const getFullTowerData = (worker:TWorker): ITower => data.content.find((t) => t.id === worker.Id)!;
+
   const towers: ITower[] = workers.map((worker) => ({
-    id: worker.Id,
-    level: DeveloperLevel.junior,
-    type: TowerType.frontend,
+    ...getFullTowerData(worker),
     position: {
       x: worker.Coordinate.X,
       y: worker.Coordinate.Y,
     },
-  }));
+  })).filter((t) => t.id);
   return (
     <>
       {towers.map((tower) => <Tower key={tower.id} {...tower} />)}
