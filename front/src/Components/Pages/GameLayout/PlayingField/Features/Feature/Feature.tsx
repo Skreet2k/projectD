b184c361cfe4e7139af4869af81370c9f3820881
Iaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-09-03 14:09:26 +0400

    feature fix

--- front/src/Components/Pages/GameLayout/PlayingField/Features/Feature/Feature.tsx
+++ front/src/Components/Pages/GameLayout/PlayingField/Features/Feature/Feature.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { LinearProgress } from '@mui/material';
 import { TFeatureProps } from './Feature.types';
+import { GameLayoutContext } from '../../../../../Providers/GameLayoutProvider/GameLayoutProvider';
 
-const FeatureWrapper = styled.div`
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  background-color: gray;   
-  line-height: 70px;
+const FeatureWrapper = styled.div<{ $width: number }>`
+  width: ${(props) => `${props.$width}px`};
+  height: ${(props) => `${props.$width}px`};
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
 `;
-export default function Feature({ name }: TFeatureProps) {
+
+const FeatureBody = styled.div`
+  border-radius: 5px;
+  background-color: #fff;
+  border-style: solid;
+  border-width: 1px;
+  padding: 5px;
+  font-size: 11px;
+  word-wrap: break-word;
+`;
+export default function Feature({ name, progress }: TFeatureProps) {
+  const { sizes } = useContext(GameLayoutContext);
+  const size = sizes?.sizeOfFieldCell || 70;
+
   return (
-    <FeatureWrapper>
-      {name}
+    <FeatureWrapper $width={size - 5}>
+      <LinearProgress variant="determinate" value={progress} color="error" />
+      <FeatureBody>
+        {name}
+      </FeatureBody>
     </FeatureWrapper>
   );
 }
